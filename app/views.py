28fb41030df1d4b74d0e1d14db27ee0b7d0b432d
Nox Dineen <noxdineen@gmail.com>
2013-03-02 23:49:25 -0500

    Basic templates, login form (that doesn't work yet)

---
+++ app/views.py
@@ -0,0 +1,35 @@
+from flask import render_template, flash, redirect
+from app import app
+from forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Captain Tightpants' }
+    posts = [
+        {
+            'author': { 'nickname': 'Jayne' },
+            'body': 'Vera is my favourite gun!'
+        },
+        {
+            'author': { 'nickname': 'Simon' },
+            'body': 'I have a stick up my butt.'
+        }
+    ]
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + ' remember_me='
+        + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
+
