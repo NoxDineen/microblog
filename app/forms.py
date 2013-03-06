c7a20f97941370f8055dffc0a431e7d39f8bae6e
Nox Dineen <noxdineen@gmail.com>
2013-03-05 22:00:32 -0500

    Added OpenID authentication, Gravatar, user profile and profile editing.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,11 @@
-from flask.ext.wtf import Form, TextField, BooleanField, Required
+from flask.ext.wtf import Form, TextField, TextAreaField, BooleanField, Required, Length
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
+
+
+class EditForm(Form):
+    nickname = TextField('nickname', validators = [Required()])
+    about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
 
